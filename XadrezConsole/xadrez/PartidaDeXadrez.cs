8fb54746d167c263e6e561427af575af8cfd62ab
Guilherme-Daneliv23 <gui.professional.profile@gmail.com>
2024-07-17 10:55:38 -0300

    Adicionando jogada especial: promoção

--- XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -144,6 +144,18 @@ namespace xadrez
 
             Peca p = tab.peca(destino);
 
+            // JOGADAESPECIAL promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
